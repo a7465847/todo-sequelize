7c906561bdf3e8fcd9cbef17b88137122fbc439d
Song Shao Ting <qwe820610@gmail.com>
2021-01-29 16:53:46 +0800

    feat: add detail page

--- app.js
+++ app.js
@@ -15,35 +15,41 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => {
-    return Todo.findAll({
-      raw: true,
-      nest: true
-    })
-      .then((todos) => { return res.render('index', { todos: todos }) })
-      .catch((error) => { return res.status(422).json(error) })
+  return Todo.findAll({
+    raw: true,
+    nest: true
   })
-  
-  app.get('/users/login', (req, res) => {
-    res.render('login')
-  })
-  
-  app.post('/users/login', (req, res) => {
-    res.send('login')
-  })
-  
-  app.get('/users/register', (req, res) => {
-    res.render('register')
-  })
-  
-  app.post('/users/register', (req, res) => {
-    const { name, email, password, confirmPassword } = req.body
-    User.create({ name, email, password })
-      .then(user => res.redirect('/'))
-  })
-  
-  app.get('/users/logout', (req, res) => {
-    res.send('logout')
-  })
+    .then((todos) => { return res.render('index', { todos: todos }) })
+    .catch((error) => { return res.status(422).json(error) })
+})
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id
+  return Todo.findByPk(id)
+    .then(todo => res.render('detail', { todo: todo.toJSON() }))
+    .catch(error => console.log(error))
+})
+
+app.get('/users/login', (req, res) => {
+  res.render('login')
+})
+
+app.post('/users/login', (req, res) => {
+  res.send('login')
+})
+
+app.get('/users/register', (req, res) => {
+  res.render('register')
+})
+
+app.post('/users/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  User.create({ name, email, password })
+    .then(user => res.redirect('/'))
+})
+
+app.get('/users/logout', (req, res) => {
+  res.send('logout')
+})
 
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
