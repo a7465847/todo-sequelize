cb271032cb45b4957f8e8fd4115d1a7edfed07e2
Song Shao Ting <qwe820610@gmail.com>
2021-01-31 00:56:03 +0800

    feat: add update todo function

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -31,4 +31,32 @@ router.get('/:id', async (req, res) => {
   }
 })
 
+// 進入編輯頁面
+router.get('/:id/edit', async (req, res) => {
+  try {
+    const UserId = req.user.id
+    const id = req.params.id
+    const todo = await Todo.findOne({ where: { id, UserId } })
+    return res.render('edit', { todo: todo.toJSON() })
+  } catch (err) {
+    console.log(err)
+  }
+})
+
+// 編輯一筆資料
+router.put('/:id', async (req, res) => {
+  try {
+    const UserId = req.user.id
+    const id = req.params.id
+    const { name, isDone } = req.body
+    const todo = await Todo.findOne({ where: { id, UserId } })
+    todo.name = name
+    todo.isDone = isDone === 'on'
+    todo.save()
+    return res.redirect(`/todos/${id}`)
+  } catch (err) {
+    console.log(err)
+  }
+})
+
 module.exports = router
